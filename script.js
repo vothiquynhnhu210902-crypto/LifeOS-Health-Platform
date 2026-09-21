8567958396e15d22494e7e08287a07390277ba23
vothiquynhnhu210902-crypto <vothiquynhnhu210902@gmail.com>
2026-09-22 00:15:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ defaultPatients.forEach(defaultPatient => {
 localStorage.setItem(
     "lifeosPatients",
     JSON.stringify(patients)
-
+);
 
 // ========================================
 // TRA CỨU BỆNH NHÂN
